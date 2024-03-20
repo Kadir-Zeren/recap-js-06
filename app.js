c39ea0b42ab652dad04f88e9094f93196a8b846b
kadir-zeren <k4dzeren@gmail.com>
2024-03-20 12:43:10 +0300

    recursion

--- app.js
+++ app.js
@@ -11,40 +11,54 @@
 // console.log("VOLUME:", volume(r, h).toPrecision(4));
 // console.log("VOLUME:", volume(r, h).toExponential(5));
 
-const add = (n1, n2) => n1 + n2;
-const sub = (n1, n2) => n1 - n2;
-const mul = (n1, n2) => n1 * n2;
-const div = (n1, n2) => n1 / n2;
+// const add = (n1, n2) => n1 + n2;
+// const sub = (n1, n2) => n1 - n2;
+// const mul = (n1, n2) => n1 * n2;
+// const div = (n1, n2) => n1 / n2;
 
-const compute = (n1, n2, operator) => {
-  let result = 0;
+// const compute = (n1, n2, operator) => {
+//   let result = 0;
 
-  switch (operator) {
-    case "+":
-      result = add(n1, n2);
-      break;
-    case "-":
-      result = sub(n1, n2);
+//   switch (operator) {
+//     case "+":
+//       result = add(n1, n2);
+//       break;
+//     case "-":
+//       result = sub(n1, n2);
 
-      break;
-    case "x":
-    case "*":
-      result = mul(n1, n2);
-      break;
-    case "/":
-      result = div(n1, n2);
-      break;
+//       break;
+//     case "x":
+//     case "*":
+//       result = mul(n1, n2);
+//       break;
+//     case "/":
+//       result = div(n1, n2);
+//       break;
 
-    default:
-      alert("Incorrect Operator");
-      result = "Error";
-      break;
+//     default:
+//       alert("Incorrect Operator");
+//       result = "Error";
+//       break;
+//   }
+//   return result;
+// };
+
+// const n1 = +prompt("First Number:");
+// const operator = prompt("Operator:");
+// const n2 = +prompt("Second Number:");
+
+// console.log(`${n1}${operator}${n2}= ${compute(n1, n2, operator)}`);
+
+const fakto = (n) => {
+  let f = 1;
+  for (let i = n; i > 1; i--) {
+    f = f * i;
   }
-  return result;
+  return f;
 };
 
-const n1 = +prompt("First Number:");
-const operator = prompt("Operator:");
-const n2 = +prompt("Second Number:");
+const number = +prompt("n:");
 
-console.log(`${n1}${operator}${n2}= ${compute(n1, n2, operator)}`);
+number < 0
+  ? alert("Number should be bigger than zero")
+  : console.log(`Fakto(${number})= ${fakto(number)}`);
